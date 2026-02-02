ea9e426ab001a3799b9e6b16ffb6edd8b9fdaa5e
gianacevedof <gianacevedof@gmail.com>
2026-02-02 12:43:29 -0500

    First project taking input form user.

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+print("Hello there! Today you're gonna try to complete a story. ")
+print("Please, fill out the blank spaces where needed.")
+
+
+print(f"\nToday I went to a _ zoo.")
+adjective1 = input("Insert and adjective: ")
+
+print(f"\nToday I went to a {adjective1} zoo. In an exhibit, I saw a _")
+noun = input("insert a noun: ")
+
+print(f"\nToday I went to a {adjective1} zoo. In an exhibit, I saw a {noun}.")
+print(f"The {noun} was _ and _ing.")
+adjective2 = input("Insert another adjective: ")
+verb = input("insert a verb: ")
+
+print(f"\nToday I went to a {adjective1} zoo. In an exhibit, I saw a {noun}.")
+print(f"The {noun} was {adjective2} and {verb}ing.")
